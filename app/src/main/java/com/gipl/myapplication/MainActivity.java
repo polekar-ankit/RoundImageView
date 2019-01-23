2800cd598f4b2a06bc173fd63ba7644ecf2adb26
polekar.ankit <polekar.ankit@gmail.com>
2019-01-23 17:05:46 +0530

    Initial commit

---
+++ app/src/main/java/com/gipl/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.gipl.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RoundImageView roundImageView = findViewById(R.id.riv);
+        Glide.with(this)
+                .load("https://lh3.googleusercontent.com/ZaMW_i_hIto6DVk92Bj95qq3gT8Y0NtcGXWDlMW2Jnmpc3O1K_vBupktbKjbsRRhPmz1rZ8eXmUyhWOdQt5y8zlaNXqYrp0CfK0")
+                .into(roundImageView);
+
+    }
+}
